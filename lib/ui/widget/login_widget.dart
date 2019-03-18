b53063882efc756b3be95cc27b07a45007b87608
liwei <curmido@gmail.com>
2019-03-18 19:58:59 +0800

    fix

--- lib/ui/widget/login_widget.dart
+++ lib/ui/widget/login_widget.dart
@@ -15,66 +15,66 @@ import '../../top_config.dart';
 Widget loginContainer(BuildContext context, AnimationController _animationController) {
   return Center(
     child: Container(
-      margin: EdgeInsets.only(top: dimen80),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            S.of(context).appName,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: loginTextSize,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              S.of(context).appName,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: loginTextSize,
+              ),
             ),
-          ),
-          SizedBox(
-            height: dimen50,
-          ),
-          Container(
-            padding: EdgeInsets.all(dimen15),
-            decoration: BoxDecoration(
-              color: loginContainerColor,
-              borderRadius: BorderRadius.all(Radius.circular(dimen10)),
+            SizedBox(
+              height: dimen50,
             ),
-            height: loginContainerHeight,
-            width: loginContainerWidth,
-            alignment: AlignmentDirectional.center,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                TextField(
-                  autofocus: true,
-                  maxLines: 1,
-                  decoration: InputDecoration(
-                    labelText: S.of(context).userName,
-                    prefixIcon: Icon(Icons.person),
+            Container(
+              padding: EdgeInsets.all(dimen15),
+              decoration: BoxDecoration(
+                color: loginContainerColor,
+                borderRadius: BorderRadius.all(Radius.circular(dimen10)),
+              ),
+              height: loginContainerHeight,
+              width: loginContainerWidth,
+              alignment: AlignmentDirectional.center,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  TextField(
+                    autofocus: true,
+                    maxLines: 1,
+                    decoration: InputDecoration(
+                      labelText: S.of(context).userName,
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                    onChanged: (s) {
+                      gitHubProvide.userName = s;
+                    },
                   ),
-                  onChanged: (s) {
-                    gitHubProvide.userName = s;
-                  },
-                ),
-                SizedBox(
-                  height: dimen10,
-                ),
-                TextField(
-                  obscureText: true,
-                  maxLines: 1,
-                  decoration: InputDecoration(
-                    labelText: S.of(context).passWord,
-                    prefixIcon: Icon(Icons.https),
+                  SizedBox(
+                    height: dimen10,
                   ),
-                  onChanged: (s) {
-                    gitHubProvide.passWord = s;
-                  },
-                ),
-                SizedBox(
-                  height: dimen40,
-                ),
-                Provide<GitHubProvide>(
-                    builder: (context, child, GitHubProvide provide) {
-                  return InkWell(
-                    onTap: provide.loading
-                        ? null
-                        : () {
+                  TextField(
+                    obscureText: true,
+                    maxLines: 1,
+                    decoration: InputDecoration(
+                      labelText: S.of(context).passWord,
+                      prefixIcon: Icon(Icons.https),
+                    ),
+                    onChanged: (s) {
+                      gitHubProvide.passWord = s;
+                    },
+                  ),
+                  SizedBox(
+                    height: dimen40,
+                  ),
+                  Provide<GitHubProvide>(
+                      builder: (context, child, GitHubProvide provide) {
+                        return InkWell(
+                          onTap: provide.loading
+                              ? null
+                              : () {
                             if (provide.userName.isNotEmpty &&
                                 provide.passWord.isNotEmpty) {
                               provide.gitLogin().doOnListen(() {
@@ -97,24 +97,25 @@ Widget loginContainer(BuildContext context, AnimationController _animationContro
                               globalKey.currentState.showSnackBar(snackBar);
                             }
                           },
-                    child: Container(
-                      height: dimen48,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: loginColor,
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(dimen2)),
-                      ),
-                      width: provide.loginBtnWidth,
-                      alignment: AlignmentDirectional.center,
-                      child: _loginButtonChild(context, provide),
-                    ),
-                  );
-                }),
-              ],
-            ),
-          )
-        ],
+                          child: Container(
+                            height: dimen48,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: loginColor,
+                              ),
+                              borderRadius: BorderRadius.all(Radius.circular(dimen2)),
+                            ),
+                            width: provide.loginBtnWidth,
+                            alignment: AlignmentDirectional.center,
+                            child: _loginButtonChild(context, provide),
+                          ),
+                        );
+                      }),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     ),
   );
