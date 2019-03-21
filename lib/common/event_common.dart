b111352217841a3680c16e2d2ba4c3f4c09b184c
liwei <curmido@gmail.com>
2019-03-21 17:33:36 +0800

    fix

---
+++ lib/common/event_common.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+/*
+ * @Date: 2019-03-21 16:57 
+ * @Description TODO
+ */
+
+
+const EventType = {
+  'CheckRunEvent':'',
+  'CheckSuiteEvent':'',
+  'CommitCommentEvent':'',
+  'ContentReferenceEvent':'',
+  'CreateEvent':'',
+  'DeleteEvent':'',
+  'DeploymentEvent':'',
+  'DeploymentStatusEvent':'',
+  'DownloadEvent':'',
+  'FollowEvent':'',
+  'ForkEvent':'',
+  'ForkApplyEvent':'',
+  'GitHubAppAuthorizationEvent':'',
+  'GistEvent':'',
+  'GollumEvent':'',
+  'InstallationEvent':'',
+  'InstallationRepositoriesEvent':'',
+  'IssueCommentEvent':'',
+  'IssuesEvent':'',
+  'LabelEvent':'',
+  'MarketplacePurchaseEvent':'',
+  'MemberEvent':'',
+  'MembershipEvent':'',
+  'MilestoneEvent':'',
+  'OrganizationEvent':'',
+  'OrgBlockEvent':'',
+  'PageBuildEvent':'',
+  'ProjectCardEvent':'',
+  'ProjectColumnEvent':'',
+  'ProjectEvent':'',
+  'PublicEvent':'',
+  'PullRequestEvent':'',
+  'PullRequestReviewEvent':'',
+  'PullRequestReviewCommentEvent':'',
+  'PushEvent':'',
+  'ReleaseEvent':'',
+  'RepositoryEvent':'',
+  'RepositoryImportEvent':'',
+  'RepositoryVulnerabilityAlertEvent':'',
+  'SecurityAdvisoryEvent':'',
+  'StatusEvent':'',
+  'TeamEvent':'',
+  'TeamAddEvent':'',
+  'WatchEvent':'',
+};
